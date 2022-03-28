c71bd2a034cdef43f5249181fa985118c3c382e2
lyj0158 <1162168108@qq.com>
2022-03-28 21:05:49 +0800

    建立uni-Shop项目

---
+++ pages/center/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+    我的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped> 
+
+</style>
